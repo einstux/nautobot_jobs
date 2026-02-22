e201f9b2368b69b61703faa64f6b47351208984e
Tom Merlin Eichhorn <tom@wirkbetrieb.net>
2026-02-22 17:55:38 +0100

    Add reverse zone generation

--- jobs/dns.py
+++ jobs/dns.py
@@ -1,6 +1,9 @@
+import ipaddress
+import json
 from nautobot.apps import jobs
-from nautobot.ipam.models import IPAddressToInterface
+from nautobot.ipam.models import IPAddressToInterface, Prefix
 from fqdn import FQDN
+import desec.api
 
 name = "DNS Tasks"
 
@@ -62,4 +65,79 @@ class SetDNSEntryOnIP(jobs.Job):
         return IPAddressToInterface.objects.all()
 
 
+class SyncReverseZones(jobs.Job):
+    class Meta:
+        name = "Syncs the entries from prefixes to the right reverse zone"
+        has_sensitive_variables = False
+        is_singleton = True
+
+    def __init__(self):
+        super().__init__()
+        self.logger.debug("Reading the token")
+        with open("/opt/nautobot/secrets/desec.json", "r") as datafile:
+            desec_secret = json.load(datafile)
+        self.desec_client = desec.api.APIClient(desec_secret["token"])
+
+    def run(self):
+        prefixes = self.get_prefixes_with_reverse_zone()
+        for prefix in prefixes:
+            self.get_all_entries_from_prefix_and_update_zone(prefix)
+
+    def get_prefixes_with_reverse_zone(self):
+        self.logger.debug("Getting prefixes with reverse zone")
+        reverse_prefixes = []
+        pa = Prefix.objects.all()
+        for p in pa:
+            if p.cf["reverse_zone"] is not None:
+                reverse_prefixes.append(p)
+        return reverse_prefixes
+
+    def get_all_entries_from_prefix_and_update_zone(self, prefix):
+        self.logger.debug(
+            f"Processing Prefix: {prefix}, Test Mode: {self.run_arpa_test}",
+            extra={"object": prefix},
+        )
+
+        reverse_zone_name = prefix.cf["reverse_zone"]
+        rr_entries = []
+
+        ips = prefix.all_ips.filter(dns_name__isnull=False)
+
+        for ip in ips:
+            if len(ip.dns_name) == 0:
+                continue
+            python_ip = ipaddress.ip_address(ip.host)
+            reverse_entry = python_ip.reverse_pointer
+
+            reverse_entry = reverse_entry.replace(f".{reverse_zone_name}", "")
+            self.logger.debug(
+                f"Processing IP: {ip}, Reverse Pointer is: {reverse_entry}",
+                extra={"object": ip},
+            )
+
+            entry = {
+                "subname": reverse_entry,
+                "type": "PTR",
+                "ttl": 3600,
+                "records": [ip.dns_name],
+            }
+            rr_entries.append(entry)
+
+        rr_entries.append(
+            {
+                "subname": "",
+                "type": "NS",
+                "ttl": 3600,
+                "records": ["ns1.desec.io.", "ns2.desec.org."],
+            }
+        )
+
+        self.logger.debug(rr_entries, extra={"object": prefix})
+
+        self.desec_client.update_bulk_record(
+            domain=reverse_zone_name, rrset_list=rr_entries, exclusive=True
+        )
+
+
 jobs.register_jobs(SetDNSEntryOnIP)
+jobs.register_jobs(SyncReverseZones)
